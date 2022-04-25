25382e85566e9c577fa75bbecc654e4c29e1ee02
moeketsi <raymondkxng17@gmail.com>
2022-04-25 14:49:01 +0200

    images and burger toggle

---
+++ ap.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.navlinks');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('open');
+})
+
+
+
+
+
+
